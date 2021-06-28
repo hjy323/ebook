e6c44ca0636060fbf0a1b0df36df76cf3b7578b0
hejiayang <1350459461@qq.com>
2021-06-28 15:41:33 +0800

    ebook

---
+++ src/components/home/SearchBar.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="search-bar">
+    <div class="search-bar-title-wrapper" v-show="titleVisible">
+      <div class="title-icon-back-wrapper">
+        <span class="icon-back icon"></span>
+      </div>
+      <div class="title-text-wrapper">
+        <span class="title-text title">{{ $t("home.title") }}</span>
+      </div>
+      <div class="title-icon-shake-wrapper">
+        <span class="icon-shake icon"></span>
+      </div>
+    </div>
+    <div class="search-bar-input-wrapper">
+      <div class="search-bar-input">
+        <span class="icon-search icon"></span>
+        <input
+          type="text"
+          class="input"
+          :placeholder="$t('home.hint')"
+          v-model="searchText"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { storeHomeMixin } from '../../utils/mixin'
+export default {
+    mixins: [ storeHomeMixin ],
+  data() {
+    return {
+      searchText: "",
+      titleVisible: true
+    }
+  },
+  watch: {
+      offsetY(offsetY) {
+          console.log(offsetY)
+      }
+  }
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+@import "../../assets/styles/global";
+
+.search-bar {
+  position: relative;
+  width: 100%;
+  height: px2rem(94);
+  .search-bar-title-wrapper {
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: px2rem(42);
+    .title-icon-back-wrapper {
+      position: absolute;
+      left: px2rem(15);
+      top: 0;
+      height: px2rem(42);
+      @include center;
+    }
+    .title-text-wrapper {
+      width: 100%;
+      height: px2rem(42);
+      @include center;
+    }
+    .title-icon-shake-wrapper {
+      position: absolute;
+      right: px2rem(15);
+      top: 0;
+      height: px2rem(42);
+      @include center;
+    }
+  }
+  .search-bar-input-wrapper {
+    width: 100%;
+    height: px2rem(52);
+    padding: px2rem(10);
+    box-sizing: border-box;
+    .search-bar-input {
+      border-radius: px2rem(20);
+      width: 100%;
+      background: #f4f4f4;
+      @include left;
+      padding: px2rem(5) px2rem(15);
+      box-sizing: border-box;
+      border: px2rem(1) solid #eee;
+      .icon-search {
+          color: #999;
+      }
+      .input {
+          width: 100%;
+          height: px2rem(22);
+          border: none;
+          background: transparent;
+          margin-left: px2rem(10);
+          font-size: px2rem(12);
+          color: #666;
+          &:focus {
+              outline: none;
+          }
+          &::-webkit-input-placeholder {
+              color: #ccc;
+          }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/store/StoreHome.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="store-home">
+        <search-bar></search-bar>
+        <scroll :top="94" @onScroll="onScroll">
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+            <div>11111111111111111</div>
+        </scroll>
+    </div>
+</template>
+
+<script>
+import { storeHomeMixin } from '../../utils/mixin'
+import SearchBar from '../../components/home/SearchBar.vue'
+import Scroll from "../../components/Scroll.vue"
+export default {
+    mixins: [ storeHomeMixin ],
+    components: {
+        SearchBar,
+        Scroll
+    },
+    methods: {
+        onScroll(offsetY) {
+            this.setOffsetY(offsetY)
+        }
+    }
+}
+</script>
+
+<style  scoped>
+
+.store-home {
+    width: 100%;
+    height: 100%;
+}
+</style>
